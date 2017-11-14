b24b7fdc319ac13912d4daa1e6cf26e323760e16
Anastasia Lucas <ani@theta.local>
2017-11-14 08:38:26 -0500

    json

---
+++ liri.js
@@ -0,0 +1,19 @@
+var keyList = require("./keys.js");
+
+
+var fs = require("fs");
+var twitter = require("twitter");
+var spotify = require("spotify");
+var request = require("request");
+
+//node liri.js my-tweets
+//show last 20 tweets and when they were created in terminal
+
+//node liri.js spotify-this-song '<song name>'
+//show artist, song name, preview link of song, album
+
+//if no song, default to "the Sign" by ace of base
+
+//movie-this
+
+//do-what-it-says

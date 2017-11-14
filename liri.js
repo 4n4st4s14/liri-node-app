84c7cc3d9280ca34304356ee30ec7ee7d503d5cb
Anastasia Lucas <ani@theta.local>
2017-11-14 09:56:47 -0500

    twitter func

--- liri.js
+++ liri.js
@@ -8,7 +8,30 @@ var request = require("request");
 
 //node liri.js my-tweets
 //show last 20 tweets and when they were created in terminal
+var myTweets = function() {
+  var client = new twitter(keyList.twitterKeys);
 
+  var params= {screen_name: "mildred", count: 10};
+
+  //array for tweets to get pushed to
+  var tweetsArr= [];
+
+  client.get('statuses/user_timeline', params, function(error, tweets, response){
+    if (!error) {
+
+      for (var i = 0; i < tweets.length; i++){
+        tweetsArr.push ({
+          //tweets time and text?
+        })
+      }
+      console.log(tweetsArr)
+    }
+  });
+
+  //get?
+}
+
+myTweets();
 //node liri.js spotify-this-song '<song name>'
 //show artist, song name, preview link of song, album
 

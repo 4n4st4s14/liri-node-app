ed9df11ee956693d4a46ce52a64804b1de6a014d
Anastasia Lucas <ani@theta.local>
2017-11-14 21:28:56 -0500

    movie func

--- keys.js
+++ keys.js
@@ -5,6 +5,6 @@ var twitterKeys = {
   consumer_secret: 'nJjSiBexKjVfXk0NJQcxS3XwEzaMyXlJmOnWLyq7v8rzZP4jp2',
   access_token_key: '930249865338195968-vHB7Q0TriKuB5SHRH9F4oMd3zBbIHBG',
   access_token_secret: 'rG6UNaiKOK0lMMNAj20BSFgvcpo42oIcuJgd7NOsByzUX',
-}
+};
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -11,27 +11,75 @@ var request = require("request");
 var myTweets = function() {
   var client = new twitter(keyList.twitterKeys);
 
-  var params= {screen_name: "mildred", count: 10};
+  var params= {screen_name: "nodejs", count: 10};
 
-  //array for tweets to get pushed to
-  var tweetsArr= [];
-
-  client.get('statuses/user_timeline', params, function(error, tweets, response){
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
+        var data = []; //empty array to hold data
+        for (var i = 0; i < tweets.length; i++) {
+          data.push({
+              'created at: ' : tweets[i].created_at,
+              'Tweets: ' : tweets[i].text,
+          });
+        }
+        console.log(data);
+  }
+});
+}; //end twitter function
 
-      for (var i = 0; i < tweets.length; i++){
-        tweetsArr.push ({
-          //tweets time and text?
-        })
-      }
-      console.log(tweetsArr)
+
+//spotify function
+
+var getSpotify = function(songName) {
+  //if can't find it, The Sign by Ace of base
+  if (songName === undefined){
+    songName = 'The Sign';
+  };
+
+  // npm spotify search
+  spotify.search({ type: 'track', query: songName }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+
+console.log(data);
+});
+}// end of spotify function
+
+var getMovie = function(movieName) {
+  if (movieName === undefined){
+    movieName = 'Mr Nobody';
+  }
+
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+  request(queryUrl, function(error, response, body){
+    if (!error && response.statusCode == 200) {
+      var data = [];
+      var jsonData = JSON.parse(body);
+
+      data.push({
+        'Title: ': jsonData.Title,
+        'Year: ': jsonData.Year,
+        'IMDB Rating: ': jsonData.imdbRating,
+        'Rotten Tomatoes Rating: ': jsonData.Ratings[1].Value,
+        'Country Produced In: ': jsonData.Country,
+        'Language: ' : jsonData.Language,
+        'Plot: ' : jsonData.Plot,
+        'Actors: ' : jsonData.Actors,
+      })
+      console.log(data);
     }
   });
+}//end movie function
+
+
+
 
   //get?
-}
 
-myTweets();
+
+
 //node liri.js spotify-this-song '<song name>'
 //show artist, song name, preview link of song, album
 
@@ -40,3 +88,37 @@ myTweets();
 //movie-this
 
 //do-what-it-says
+
+//switch statement for user input
+
+var cmd = function(userCmd, userData){
+  switch (userCmd){
+    case 'my-tweets':
+    myTweets();
+    break;
+
+    case 'spotify-this-song':
+    getSpotify(userData);
+    break;
+
+    case 'movie-this':
+    getMovie(userData);
+    break;
+
+    case 'do-what-it-says':
+    doWhatItSays(userData);
+    break;
+
+    default:
+    console.log("Liri doesn\'t know that :(")
+  }
+}
+
+//variable assigned to function that that takes 2 arguments as paramemters and
+//sends them to the cmd function
+
+var argCatcher = function(argOne, argTwo){
+  cmd(argOne, argTwo);
+}
+
+argCatcher(process.argv[2], process.argv[3]);
